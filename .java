c3571ac5548e36b5846ba932c22f0ba6b66125c5
Ethan Wayne <72370523+ewayne08@users.noreply.github.com>
2025-10-15 13:57:05 +0100

    Update .java

--- .java
+++ .java
@@ -1,5 +1,16 @@
+// HelloWorld.java
 public class HelloWorld {
+    /**
+     * Main entry point
+     */
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello World");
+    }
+    
+    /**
+     * Greets a person
+     */
+    public String greet(String name) {
+        return "Hello, " + name;
     }
 }
